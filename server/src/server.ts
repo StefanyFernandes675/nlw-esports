e5bef6cbe63b6e7d5aba52426a42f02d8905f27d
StefanyFernandes675 <stefanyfernandes675@gmail.com>
2022-09-14 21:52:01 -0300

    atualizado as paginas web e mobile. Adicionado arquivo .gitigonre

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/games', (request, response) => {
+    return response.json([])
+});
+
+app.post('/ads', (request, response) => {
+    return response.json([])
+});
+
+app.get('/games/:id/ads', (request, response) =>{
+    return response.json([
+        {id: 1, name: "Anúncio 1"},
+        {id: 2, name: "Anúncio 2"},
+        {id: 3, name: "Anúncio 3"},
+    ])
+});
+
+app.get('/ads/:id/discord', (request, response) =>{
+    return response.json([])
+});
+
+app.listen(3333);
